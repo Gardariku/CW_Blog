ba86bf234f5df6c30c2874c24b6ff92f81bf432e
Gardariku <gardariku@yandex.ru>
2019-01-11 14:15:23 +0300

    client-server v1.01

--- src/server/Database.java
+++ src/server/Database.java
@@ -1,14 +1,39 @@
 package server;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
 import java.sql.*;
 
 public class Database {
     Connection connection = null;
     ResultSet resultSet = null;
 
-    public void connect() throws ClassNotFoundException, SQLException {
+    /*public void connect() throws ClassNotFoundException, SQLException {
         Class.forName("org.sqlite.JDBC");
         connection = DriverManager.getConnection("jdbc:sqlite:Chat.db");
+    }*/
+    public void connect() throws ClassNotFoundException, SQLException {
+        Context initContext = null;
+        try {
+            initContext = new InitialContext();
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        Context envContext  = null;
+        try {
+            envContext = (Context)initContext.lookup("java:/comp/env");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        DataSource dataSource = null;
+        try {
+            dataSource = (DataSource)envContext.lookup("jdbc/chat");
+        } catch (NamingException e) {
+            e.printStackTrace();
+        }
+        Connection connection = dataSource.getConnection();
     }
 
 }
